260f4d5884074854124b4475cca1082fd9f2b400
subayadhav <suba10499@gmail.com>
2019-02-03 20:50:03 +0530

    Create Factor.java

---
+++ Factor.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+  public class Factor{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the Range:");
+    int rangee=sc.nextInt();
+    for(int i=0;i<range;i++)
+    {
+    if(rangee%i==0)
+    {
+    System.out.println(i);
+    }
+    }
+   }
+} 
