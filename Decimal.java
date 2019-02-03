21f12cf1abbc566c948420b15dd79b0b85df865e
subayadhav <suba10499@gmail.com>
2019-02-03 20:39:43 +0530

    Create Decimal.java

---
+++ Decimal.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+  public class Decimal{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter Any Number:");
+    double a=sc.nextDouble();
+    System.out.println(" "+(int)Math.ceil(a));
+    }
+} 
