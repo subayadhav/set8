5db3ec240c875cda88e68c70f6de50acf251d1a2
subayadhav <suba10499@gmail.com>
2019-02-03 20:51:02 +0530

    Create Multiple.java

---
+++ Multiple.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+  public class Multiple{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter any integer:");
+    int num1=sc.nextInt();
+    if(num1%13==0)
+    {
+    System.out.println("YES");
+    }
+    else{
+    SYstem.out.println("NO");
+    }
+   }
+} 
